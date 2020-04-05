ac25740fffda6995f7c5c58ab81444ca87f43963
AMIT VERMA <55926153+amitverma07@users.noreply.github.com>
2020-04-04 22:27:07 -0500

    Updates

---
+++ precache-manifest.ac033b7b79c84717d6e2fd7e08830351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1453be250cc1eba1ab5b35e0ce47207c",
+    "url": "/COVID-19-TRACKER/index.html"
+  },
+  {
+    "revision": "25897e5838ee74620cd5",
+    "url": "/COVID-19-TRACKER/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "240cf700d09014c38495",
+    "url": "/COVID-19-TRACKER/static/js/2.a07d641d.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/COVID-19-TRACKER/static/js/2.a07d641d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25897e5838ee74620cd5",
+    "url": "/COVID-19-TRACKER/static/js/main.f371fc15.chunk.js"
+  },
+  {
+    "revision": "eae1da0223de26af20fe",
+    "url": "/COVID-19-TRACKER/static/js/runtime-main.94b250b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/COVID-TRACKER/precache-manifest.f34663991f9ba3354343a14b178a72b2.js"
+  "/COVID-19-TRACKER/precache-manifest.ac033b7b79c84717d6e2fd7e08830351.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-TRACKER/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/COVID-19-TRACKER/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
